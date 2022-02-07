aa7b9cf0e12c7c6494315e1f06d1b5ed8a02ac6b
Anders Culver <anders.culver@gmail.com>
2022-02-07 16:04:31 -0600

    Added packages, completed number guessing game

--- learning/the_rust_programming_language/guessing_game/src/main.rs
+++ learning/the_rust_programming_language/guessing_game/src/main.rs
@@ -1,15 +1,41 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
 
-    println!("Please input your guess.");
+    // Create secret number to guess, print it for testing purposes
+    let secret_number = rand::thread_rng().gen_range(1..101);
 
-    let mut guess = String::new();
+    //println!("The secret number is: {}", secret_number);
     
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    loop {
+        // Gather user input, convert string to number for compare
+        println!("Please input your guess.");
 
-    println!("You guessed: {}", guess);
+        let mut guess = String::new();
+        
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        // Show guess to user
+        println!("You guessed: {}", guess);
+
+        // Compare guess to secret number
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
